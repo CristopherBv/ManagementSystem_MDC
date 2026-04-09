f61148df5fb5d6b1729d2b3d3becab3942935ad2
CristopherBv <a21201372@alumnos.uady.mx>
2026-04-09 17:32:32 -0600

    se agregaron los nombres de cada tipo de dato en ProductoDAO.java en el metodo mapearDeCSV

--- src/main/java/com.gestionmats.app/dao/ProductoDAO.java
+++ src/main/java/com.gestionmats.app/dao/ProductoDAO.java
@@ -21,13 +21,18 @@ public class ProductoDAO extends AbstractCsvDAO<Producto> {
     @Override
     protected Producto mapearDeCSV(String[] datos) {
         return new Producto(
-                datos[0], datos[1], datos[2],
-                Double.parseDouble(datos[3]),
-                Integer.parseInt(datos[4]),
-                datos[5], datos[6],
-                Integer.parseInt(datos[7]),
-                Double.parseDouble(datos[8]),
-                datos[9], datos[10], datos[11]
+                datos[0], //id
+                datos[1], //nombre
+                datos[2], //descripcion
+                Double.parseDouble(datos[3]), //precio
+                Integer.parseInt(datos[4]), //stock
+                datos[5], //categoría
+                datos[6], //unidad
+                Integer.parseInt(datos[7]), //stock minimo
+                Double.parseDouble(datos[8]), //descuento
+                datos[9], //imagen
+                datos[10], //createdAt
+                datos[11] //updatedAt
         );
     }
 
