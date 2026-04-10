6ce27b398e11b322764cc25e98fb1d9cbb097faf
Herbert-Espejo <a21200821@alumnos.uady.mx>
2026-04-09 19:36:34 -0600

    Add comment for future method in IProveedorDAO

--- src/main/java/com.gestionmats.app/dao/IProveedorDAO.java
+++ src/main/java/com.gestionmats.app/dao/IProveedorDAO.java
@@ -4,4 +4,5 @@ import com.gestionmats.app.models.Proveedor;
 
 public interface IProveedorDAO extends IDAO<Proveedor> {
     // Si en el futuro se necesita un método específico, va aquí.
+    // Ejemplo: List<Proveedor> getNombreProveedor();
 }
