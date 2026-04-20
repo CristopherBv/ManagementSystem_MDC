9d407360aecf3d470bf4ba4fb71840824660bc13
CristopherBv <a21201372@alumnos.uady.mx>
2026-04-20 13:18:13 -0600

    Se crearon: Setters y Getters de AbstractUsuario.java Nota: no se si esten bien xd

--- src/main/java/gestionmats/model/AbstractUsuario.java
+++ src/main/java/gestionmats/model/AbstractUsuario.java
@@ -7,7 +7,7 @@ public abstract class AbstractUsuario {
     private String nombre;
     private String username;
     private String password;
-    private RolUsuario rol; // Usamos el Enum aquí
+    private RolUsuario rol; // Usamos el ENUM aquí
 
     public AbstractUsuario(int idUsuario, String primerApellido, String segundoApellido, String nombre, String username, String password, RolUsuario rol) {
         this.idUsuario = idUsuario;
@@ -19,8 +19,57 @@ public abstract class AbstractUsuario {
         this.rol = rol;
     }
 
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RolUsuario getRol() {
+        return rol;
+    }
+    public void setRol(RolUsuario rol) {
+        this.rol = rol;
+    }
+
     // Aquí irían todos tus Getters y Setters...
 
-    // Un método abstracto opcional si quieres polimorfismo puro
-    public abstract void mostrarMenuPrincipal();
+
 }
