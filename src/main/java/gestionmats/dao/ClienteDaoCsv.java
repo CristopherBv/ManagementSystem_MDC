5dffd5888c500f0de3d57918aa0cff2e7373e114
estefaniaesquivel <a21201099@alumnos.uady>
2026-05-09 21:45:48 -0600

    se quitó metodo innecesario

--- src/main/java/gestionmats/dao/ClienteDaoCsv.java
+++ src/main/java/gestionmats/dao/ClienteDaoCsv.java
@@ -98,16 +98,5 @@ public class ClienteDaoCsv implements Dao<Cliente> {
         return max;
     }
 
-    /**
-     * Acumula puntos al cliente después de una venta
-     * @param idCliente ID del cliente
-     * @param montoVenta Monto total de la venta
-     */
-    public void acumularPuntos(int idCliente, double montoVenta) {
-        Cliente c = buscarPorId(idCliente);
-        if (c != null) {
-            c.acumularPuntos(montoVenta);
-            actualizar(c);
-        }
-    }
+
 }
